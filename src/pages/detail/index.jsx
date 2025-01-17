46f8f3b78ed2acc6d6ab95b8fffd3575adfeffeb
cyl <2893176150@qq.com>
2025-01-17 10:11:52 +0800

    feat: :white_check_mark: 完成
    部分完成
    
    123

--- src/pages/detail/index.jsx
+++ src/pages/detail/index.jsx
@@ -1,15 +1,17 @@
-import React, { memo } from "react";
+import React, { memo, useState } from "react";
 import PropTypes from "proptypes";
 
 import { useLoad } from "@tarojs/taro";
-import { View } from "@tarojs/components";
+import { View, WebView } from "@tarojs/components";
 import styles from "./index.module.scss";
 
 const Detail = memo(() => {
+  const [link, setLink] = useState(null);
   useLoad((options) => {
     console.log("options", options);
+    setLink(options.link);
   });
-  return <View className={styles["detail"]}>Detail</View>;
+  return <WebView src={link} className={styles["detail"]}></WebView>;
 });
 
 export default Detail;
